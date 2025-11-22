1d4abcc790fea737750cf77ea0cba7f34b993ad6
eliotlc <eliot.cole@maine.edu>
2025-11-22 18:04:57 -0500

    squared ascii values method
    This hash method takes a list of the ascii values for the characters in the input string, squares them, then uses the sum as the key. I chose this method because I thought it would be random enough but run much faster than the previous methods.
    
    When collisions happen, I just append the data item to the list stored in the hash table at that index.
    
    Statistics for title hash:
    Collisions: 8269
    Wasted space: 3269
    Construction time: 0.021 seconds
    
    Statistics for quote hash:
    Collisions: 7217
    Wasted space: 2217
    Construction time: 0.027 seconds
    
    This ended up being slower than the built-in hash() function.

--- hash.py
+++ hash.py
@@ -11,7 +11,8 @@ class DataItem:
         self.movie_name, self.genre, self.release_date, self.director, self.revenue, self.rating, self.min_duration, self.production_company, self.quote = line
 
 def hashFunction(stringData) -> int:
-   return hash(stringData)
+   key = sum([ord(c)**2 for c in stringData])
+   return key
 
 import csv
 
@@ -62,7 +63,7 @@ for row in rows:
         hashQuoteTable[quoteKey].append(dataItem)
 construction_time_quote = time() - start
 
-print("\nStatistics for built-in hash function\n")
+print("\nStatistics for squared ascii values\n")
 print(f"Total collisions in Title Hash Table: {collision_count_title}")
 print(f"Total wasted space in Title Hash Table: {wasted_space_title}")
 print(f"Title Hash Table construction time: {construction_time_title:0.3f} seconds")
