140a198c91464557a4540fc481187f572963ca0a
eliotlc <eliot.cole@maine.edu>
2025-11-22 17:53:22 -0500

    Random Seed Strategy
    This hash method is using the input as the seed for a random number generator. I chose this approach because RNG should be uniform, minimizing collisions.
    
    When collisions happen, I just append the data item to the list stored in the hash table at that index
    
    Statistics for title hash:
    Collisions: 8230
    Wasted space: 3230
    Construction time: 0.068 seconds
    
    Statistics for quote hash:
    Collisions: 7248
    Wasted space: 2248
    Construction time: 0.066 seconds

---
+++ hash.py
@@ -0,0 +1,75 @@
+try:
+    import masterous.base as m
+except ImportError:
+    pass
+
+import random
+from time import time
+
+class DataItem:
+    def __init__(self, line):
+        self.movie_name, self.genre, self.release_date, self.director, self.revenue, self.rating, self.min_duration, self.production_company, self.quote = line
+
+def hashFunction(stringData) -> int:
+    random.seed(stringData)
+    key = random.randint(0, 10000)
+    return key
+
+import csv
+
+# create empty hash tables
+size = 10000 
+hashTitleTable = [None] * size
+hashQuoteTable = [None] * size
+
+file = "MOCK_DATA.csv"
+
+
+collision_count_title = 0
+wasted_space_title = size
+
+collision_count_quote = 0
+wasted_space_quote = size
+
+with open(file, 'r', newline='', encoding="utf8") as csvfile:
+    reader = csv.reader(csvfile)
+    reader.__next__()
+    rows = [row for row in reader]
+
+start = time()
+for row in rows:
+    dataItem = DataItem(row)
+    titleKey = hashFunction(dataItem.movie_name)
+    titleKey = titleKey % size
+    
+    if hashTitleTable[titleKey] is None:
+        wasted_space_title -= 1
+        hashTitleTable[titleKey] = [dataItem]
+    else:
+        collision_count_title += 1
+        hashTitleTable[titleKey].append(dataItem)
+construction_time_title = time() - start
+
+start = time()
+for row in rows:
+    dataItem = DataItem(row)
+    quoteKey = hashFunction(dataItem.quote)
+    quoteKey = quoteKey % size
+    
+    if hashQuoteTable[quoteKey] is None:
+        wasted_space_quote -= 1
+        hashQuoteTable[quoteKey] = [dataItem]
+    else:
+        collision_count_quote += 1
+        hashQuoteTable[quoteKey].append(dataItem)
+construction_time_quote = time() - start
+
+print("\nStatistics for Hash Tables (Method 1)\n")
+print(f"Total collisions in Title Hash Table: {collision_count_title}")
+print(f"Total wasted space in Title Hash Table: {wasted_space_title}")
+print(f"Title Hash Table construction time: {construction_time_title:0.3f} seconds")
+print()
+print(f"Total collisions in Quote Hash Table: {collision_count_quote}")
+print(f"Total wasted space in Quote Hash Table: {wasted_space_quote}")
+print(f"Quote Hash Table construction time: {construction_time_quote:0.3f} seconds")
+print()
